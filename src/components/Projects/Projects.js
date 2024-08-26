0bffe2c22fd18113c9e29a7bea7a31dd1451b7fa
orsolomon <orsolomon24@gmail.com>
2024-08-26 16:19:33 +0300

    another project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import chatify from "../../Assets/Projects/chatify.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import myKitchen from "../../Assets/Projects/myKitchen.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -33,6 +33,15 @@ function Projects() {
               title="LastCall"
               description="My Flight search website offers users a comprehensive solution for their travel needs, leveraging an advanced bot. Users can search for flights, read and write reviews, and gain insights into popular destinations. With a bot in place, my platform matches appropriate flights with each vacation, ensuring a seamless travel experience for all."
               ghLink="https://github.com/ohadmizrahi/LastCall"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={myKitchen}
+              isBlog={false}
+              title="MyKitchen"
+              description="a full-stack recipe management application where my girlfriend can easily add, edit, and categorize their favorite recipes. The application includes features like manual recipe entry, adding recipes via links, and organized category selection for efficient browsing. Implemented using React for the frontend and Node.js with MongoDB for the backend, focusing on a seamless and responsive user experience across devices."
+              ghLink="https://github.com/OrSolomon24/MyKitchen"
             />
           </Col>
 
